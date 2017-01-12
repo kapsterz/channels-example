8f1159fc1ece0222cb9a3764af6fc1e00f34a78c
kapsterz <wegodeboy@yandex.com>
2017-01-12 04:16:02 +0200

    ddd

--- chat/urls.py
+++ chat/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^contacts/$', views.contacts, name='contacts'),
     url(r'^new/$', views.new_room, name='new_room'),
-    url(r'^modelists/$', views.new_room, name='new_room')
     url(r'^(?P<label>[\w-]{,50})/$', views.chat_room, name='chat_room'),
 ]
