f344ba3f0890669b48c087ae22a5ed20093f6e35
kapsterz <wegodeboy@yandex.com>
2017-01-12 07:46:53 +0200

    Click added.

--- static/chat.js
+++ static/chat.js
@@ -1,27 +1,35 @@
-$(function() {
+function soundClick() {
+    var audio = new Audio();
+    audio.src = 'click.mp3';
+    audio.autoplay = true;
+}
+
+$(function () {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
     var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
-    
-    chatsock.onmessage = function(message) {
+
+    chatsock.onmessage = function (message) {
         var data = JSON.parse(message.data);
-        var chat = $("#chat")
-        var ele = $('<tr></tr>')
+        var chat = $("#chat");
+        var ele = $('<tr></tr>');
 
         ele.append(
             $("<td></td>").text(data.timestamp)
-        )
+        );
         ele.append(
             $("<td></td>").text(data.handle)
-        )
+        );
         ele.append(
             $("<td></td>").text(data.message)
-        )
-        
+        );
+
+        soundClick();
+
         chat.append(ele)
     };
 
-    $("#chatform").on("submit", function(event) {
+    $("#chatform").on("submit", function (event) {
         var message = {
             handle: $('#handle').val(),
             message: $('#message').val(),
