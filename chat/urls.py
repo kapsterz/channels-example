e7bf0039b47e344bc715d4fe37dc0757fa846f2c
kapsterz <wegodeboy@yandex.com>
2017-01-12 04:10:23 +0200

    Anded base.html.

--- chat/urls.py
+++ chat/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     url(r'^donate/$', views.donate, name='donate'),
     url(r'^about/$', views.about, name='about'),
     url(r'^contacts/$', views.contacts, name='contacts'),
-    url(r'^new/$', views.new_room, name='new_room'),
-    url(r'^modelists/$', views.new_room, name='new_room')
     url(r'^(?P<label>[\w-]{,50})/$', views.chat_room, name='chat_room'),
 ]
